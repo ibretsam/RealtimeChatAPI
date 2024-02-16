c9d8cb98d8a1edf559feb2e3c73bd751d9c2ac20
Khanh Le <masterbruculous@gmail.com>
2024-02-16 15:04:22 +0700

    Add friend-related functionality and message handling to ChatConsumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,9 @@
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 from django.core.files.base import ContentFile
-from .serializers import UserSerializer, SearchSerializer, RequestSerializer
+from .serializers import UserSerializer, SearchSerializer, RequestSerializer, FriendSerializer, MessageSerializer
 from django.db.models import Q, Exists, OuterRef
-from .models import User, Connection
+from .models import User, Connection, Message
 import base64
 import json
 
@@ -55,6 +55,18 @@ class ChatConsumer(WebsocketConsumer):
 
         elif data_source == 'request-accept':
             self.receive_accept_request(data)
+
+        elif data_source == 'friend-list':
+            self.receive_friend_list(data)
+
+        elif data_source == 'message-send':
+            self.receive_message_send(data)
+
+        elif data_source == 'message-list':
+            self.receive_message_list(data)
+
+        elif data_source == 'message-typing':
+            self.receive_message_typing(data)
 
     def receive_thumbnail(self, data):
         user = self.scope['user']
@@ -208,9 +220,142 @@ class ChatConsumer(WebsocketConsumer):
 
         self.send_group(sender_username, 'request-accept', serialized.data)
 
-        self.send_group(user.username, 'request-accept', serialized.data)
+        self.send_group(connection.receiver.username,
+                        'request-accept', serialized.data)
 
-        self.send(text_data=json.dumps({
-            'source': 'request-accept',
-            'data': serialized.data
-        }))
+        # Send the new friend to the sender
+        serialized_friend = FriendSerializer(connection, context={
+            'user': sender
+        })
+
+        self.send_group(sender_username, 'friend-new', serialized_friend.data)
+
+        # Send the new friend to the receiver
+        serialized_friend = FriendSerializer(connection, context={
+            'user': connection.receiver
+        })
+
+        self.send_group(connection.receiver.username,
+                        'friend-new', serialized_friend.data)
+
+    def receive_friend_list(self, data):
+        user = self.scope['user']
+        if not user.is_authenticated:
+            return
+
+        friends = Connection.objects.filter(
+            Q(sender=user) | Q(receiver=user),
+            accepted=True
+        ).order_by('-updated_at')
+
+        serialized = FriendSerializer(
+            friends, context={'user': user}, many=True)
+
+        self.send_group(user.username, 'friend-list', serialized.data)
+
+    def receive_message_send(self, data):
+        user = self.scope['user']
+        if not user.is_authenticated:
+            return
+
+        sender_id = data.get('senderId')
+
+        sender = User.objects.get(id=sender_id)
+        print("sender", sender.username)
+
+        connection_id = data.get('connectionId')
+
+        try:
+            connection = Connection.objects.get(id=connection_id)
+        except Connection.DoesNotExist:
+            print("Connection does not exist")
+            return
+
+        content = data.get('message')
+        print("content", content)
+
+        message = Message.objects.create(
+            connection=connection,
+            sender=sender,
+            content=content
+        )
+
+        serialized_message = MessageSerializer(message, context={
+            'user': sender
+        })
+
+        serialized_user = UserSerializer(sender)
+
+        # Send the message to the receiver
+        receiver = connection.sender
+        if user == connection.sender:
+            receiver = connection.receiver
+
+        serialized_friend = UserSerializer(receiver)
+        serialized_message_receiver = MessageSerializer(message, context={
+            'user': receiver
+        })
+
+        data = {
+            'messages': serialized_message_receiver.data,
+            'user': serialized_user.data
+        }
+
+        data_receiver = {
+            'messages': serialized_message.data,
+            'user': serialized_friend.data
+        }
+
+        self.send_group(user.username, 'message-send',
+                        data_receiver)
+
+        self.send_group(receiver.username, 'message-send',
+                        data)
+
+    def receive_message_list(self, data):
+        user = self.scope['user']
+        if not user.is_authenticated:
+            return
+
+        connection_id = data.get('connectionId')
+        page = data.get('page')
+        PAGE_SIZE = 10
+
+        try:
+            connection = Connection.objects.get(id=connection_id)
+        except Connection.DoesNotExist:
+            print("Connection does not exist")
+            return
+
+        messages = Message.objects.filter(
+            connection=connection).order_by('-created_at')[page * PAGE_SIZE:(page + 1) * PAGE_SIZE]
+
+        serialized = MessageSerializer(messages, context={
+            'user': user
+        }, many=True)
+
+        serialized_user = UserSerializer(user)
+
+        messages_count = Message.objects.filter(connection=connection).count()
+
+        next_page = page + 1 if messages_count > (page + 1) * PAGE_SIZE else 0
+
+        data = {
+            'messages': serialized.data,
+            'next': next_page,
+            'user': serialized_user.data
+        }
+
+        # Send the messages to the user
+        self.send_group(user.username, 'message-list', data)
+
+    def receive_message_typing(self, data):
+        user = self.scope['user']
+        if not user.is_authenticated:
+            return
+
+        username = data.get('username')
+
+        self.send_group(username, 'message-typing', {
+            'username': user.username
+        })
