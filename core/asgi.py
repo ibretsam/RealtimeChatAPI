0284002ca450e3a9e316f112657216e434b2825a
Khanh Le <masterbruculous@gmail.com>
2024-02-17 01:04:28 +0700

    Update asgi.py

--- core/asgi.py
+++ core/asgi.py
@@ -4,11 +4,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
+from chat.routing import websocket_urlpatterns  
 
 django_asgi_app = get_asgi_application()
 
